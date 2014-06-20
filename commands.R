4341eca19a3d619e2f32092b7737760e3379b241
Nate Thompson <natethompson114@gmail.com>
2014-06-20 11:08:04 -0400

    download setup
    Made the command system, download the files needed to the users R studio
    and run everything automatically.

--- commands.R
+++ commands.R
@@ -21,4 +21,56 @@ list.files(fwd) # Checking for files listed
 temp <- tempfile()
 download.file("http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", temp)
 unzip(temp, exdir="data")
+
+download <- function(url, ...) {
+        # First, check protocol. If http or https, check platform:
+        if (grepl('^https?://', url)) {
+                
+                # If Windows, call setInternet2, then use download.file with defaults.
+                if (.Platform$OS.type == "windows") {
+                        
+                        # If we directly use setInternet2, R CMD CHECK gives a Note on Mac/Linux
+                        seti2 <- `::`(utils, 'setInternet2')
+                        
+                        # Store initial settings, and restore on exit
+                        internet2_start <- seti2(NA)
+                        on.exit(suppressWarnings(seti2(internet2_start)))
+                        
+                        # Needed for https. Will get warning if setInternet2(FALSE) already run
+                        # and internet routines are used. But the warnings don't seem to matter.
+                        suppressWarnings(seti2(TRUE))
+                        download.file(url, ...)
+                        
+                } else {
+                        # If non-Windows, check for curl/wget/lynx, then call download.file with
+                        # appropriate method.
+                        
+                        if (nzchar(Sys.which("wget")[1])) {
+                                method <- "wget"
+                        } else if (nzchar(Sys.which("curl")[1])) {
+                                method <- "curl"
+                                
+                                # curl needs to add a -L option to follow redirects.
+                                # Save the original options and restore when we exit.
+                                orig_extra_options <- getOption("download.file.extra")
+                                on.exit(options(download.file.extra = orig_extra_options))
+                                
+                                options(download.file.extra = paste("-L", orig_extra_options))
+                                
+                        } else if (nzchar(Sys.which("lynx")[1])) {
+                                method <- "lynx"
+                        } else {
+                                stop("no download method found")
+                        }
+                        
+                        download.file(url, method = method, ...)
+                }
+                
+        } else {
+                download.file(url, ...)
+        }
+}
+
+download("https://raw.githubusercontent.com/NateThompson114/Getting-and-Cleaning-Data-Project/master/run_analysis.R", "run_analysis.R")
+
 source("run_analysis.R")
